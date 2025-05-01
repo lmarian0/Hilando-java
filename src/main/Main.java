436e316928facb8a4b9326ed3fff2d23690191e8
ThiagoB <thiago.biancucci@mi.unc.edu.ar>
2025-05-01 18:10:17 -0300

    Main "hola mundo"

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        EmpresaLogistica eCommerce = new EmpresaLogistica();
-        System.out.println(eCommerce);
-        //Proceso proceso = new EntregarPedido(eCommerce);
+        //EmpresaLogistica eCommerce = new EmpresaLogistica();
+        System.out.println("hola mundo");
+
     }
 }
