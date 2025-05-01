26849ea3ee90c07da4d3b1af7d4a0ebe417d6742
AnpanMan17 <laurasurcoenzo@gmail.com>
2025-05-01 12:36:35 -0300

    Enum Estado Pedido

--- src/main/EstadoPedido.java
+++ src/main/EstadoPedido.java
@@ -1,5 +1,6 @@
-public enum EstadoCasillero {
-    VACIO,
-    OCUPADO,
-    FUERA_SERVICIO;
-}
+public enum EstadoPedido {
+        EN_PREPARACION,   // El pedido está siendo preparado
+        EN_TRANSITO,      // El pedido está en camino
+        ENTREGADO,        // El pedido ha sido entregado
+        FALLIDO           // El pedido no pudo completarse
+    }
