fe536293260b1e35a67aef66fc85f616300fcf2c
ThiagoB <thiago.biancucci@mi.unc.edu.ar>
2025-05-01 10:54:32 -0300

    The class has been documented.

--- src/main/Proceso.java
+++ src/main/Proceso.java
@@ -1,8 +1,14 @@
 package main;
 
-public abstract class Proceso extends EmpresaLogistica implements Runnable{
-    private EmpresaLogistica eCommerce;
+/**
+ * Ejecuta procesos para la ejecucion de la EMpresa Logistica.
+ */
 
+public abstract class Proceso extends EmpresaLogistica implements Runnable{
+    private EmpresaLogistica eCommerce; // Ejecuta el eCommerce de la empresa
+
+    /**Constructor del proceso
+     * runnea un hilo*/
     public Proceso() {
         run();
     }
