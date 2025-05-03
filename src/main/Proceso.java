3d34ae1bc761923de6d7b6d3de06e7c38fc270d6
Lautaro Callovi <lautarocallovi@gmail.com>
2025-05-03 17:37:31 -0300

    Funcionamiento clase main y arreglo clase Proceso

--- src/main/Proceso.java
+++ src/main/Proceso.java
@@ -4,10 +4,8 @@ package main;
  * Ejecuta procesos para la ejecucion de la EMpresa Logistica.
  */
 
-public abstract class Proceso extends EmpresaLogistica implements Runnable {
+public abstract class Proceso implements Runnable {
     protected EmpresaLogistica eCommerce; // Ejecuta el eCommerce de la empresa
-    protected Casilleros casillero = new Casilleros();
-    protected RegistroPedidos registro = new RegistroPedidos(); //registro de pedidos
 
     /**Constructor del proceso
      * runnea un hilo*/
