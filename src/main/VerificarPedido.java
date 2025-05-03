b06d1c7003a65012fe7bdcf6df6a58ed90f0dda7
RiveraAlex <rivera.joel.alex@gmail.com>
2025-05-03 18:43:40 -0300

    Clase verificar pedido
    Misma estrucutra de despacho siguiendo los lineamientos del ejercicio

--- src/main/VerificarPedido.java
+++ src/main/VerificarPedido.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class VerificarPedido extends Proceso{
@@ -8,14 +9,46 @@ public class VerificarPedido extends Proceso{
     }
     @Override
     public void run() {
-        if (verificarDatos())
-        {//eCommerce.verificacion();} // método dentro de Empresa que hace la lógica real
-        }else {throw new Error("No se pudo verificar la entrega del pedido");}
+
+        /*
+         * Agarrar un pedido aleatorio de la lista preparacion
+         * 
+         * 
+         */
+        Pedido pedido = null;
+        List<Pedido> lista= eCommerce.getRegistro().getEntregados();
+
+        if(lista.isEmpty()){
+            try {
+                System.out.println("No hay pedidos para despachar. Esperando...");
+                wait(); // El hilo se pone en espera hasta que otro hilo notifique
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Hilo interrumpido mientras estaba en espera.");
+            }
+        }
+        else{
+            int index = ThreadLocalRandom.current().nextInt(lista.size());
+            pedido = lista.get(index);
+            if (verificarDatos() && pedido.getEstado()==EstadoPedido.ENTREGADO){
+                eCommerce.getRegistro().delEntregados(pedido);
+                eCommerce.getRegistro().addVerificados(pedido);
+            }
+            else {
+                eCommerce.getRegistro().delEntregados(pedido);
+                eCommerce.getRegistro().addFallidos(pedido);
+            }
+        }
+
+        
     }
 
-    public boolean verificarDatos(){
+   
+
+    private boolean verificarDatos(){
         ThreadLocalRandom probabilidad = ThreadLocalRandom.current();
         probabilidad.nextDouble(1);
         return probabilidad.nextDouble() <= 0.95;
     }
+    
 }
