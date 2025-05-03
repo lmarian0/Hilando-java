03e49e848d78029196f4a4a545b9e7a2d1142f1b
AnpanMan17 <laurasurcoenzo@gmail.com>
2025-05-03 18:46:29 -0300

    EntregarPedido y metodo liberar en Casillero implementados

--- src/main/EntregarPedido.java
+++ src/main/EntregarPedido.java
@@ -1,21 +1,65 @@
 package main;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.List;
+//import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 public class EntregarPedido extends Proceso{
-    public EntregarPedido(EmpresaLogistica eCommerce) {
+
+    private final RegistroPedidos registro;
+    private final int demoraE;
+    private final Random random = new Random();
+    
+
+    public EntregarPedido(EmpresaLogistica eCommerce, RegistroPedidos registro, int demoraE) {
         super(eCommerce);
+        this.registro = registro;
+        this.demoraE = demoraE;
     }
+
+
     @Override
     public void run() {
-        if (verificarDatos())
-        {//eCommerce.entrega();} // método dentro de Empresa que hace la lógica real
-        }else {throw new Error("No se pudo entregar el pedido");}
+        try {
+            while(!Thread.currentThread().isInterrupted()) {
+                TimeUnit.MILLISECONDS.sleep(demoraE);
+                procesarEntrega();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restablecer el estado de interrupción
+        }
+    }
+
+    public void procesarEntrega() {
+        List <Pedido> pedidosEnTransito = registro.getTransito();
+
+        if (pedidosEnTransito.isEmpty()) {
+            return; // No hay pedidos en tránsito
+        }
+
+        synchronized (pedidosEnTransito) {
+            if (pedidosEnTransito.isEmpty()) return;
+        
+            int indiceAleatorio = random.nextInt(pedidosEnTransito.size());
+            Pedido pedido = pedidosEnTransito.get(indiceAleatorio);
+        
+            if (verificarDatos()) { // 90% de éxito
+                registro.delTransito(pedido);
+                registro.addEntregados(pedido);
+                pedido.setEstado(EstadoPedido.ENTREGADO);
+                
+                // Liberar casillero aquí (dentro del bloque sincronizado)
+                pedido.getCasilleroAsociado().liberar();
+            } else { // 10% de fallo
+                registro.delTransito(pedido);
+                registro.addFallidos(pedido);
+                pedido.setEstado(EstadoPedido.FALLIDO);
+            }
+        }
     }
 
     public boolean verificarDatos(){
-        ThreadLocalRandom probabilidad = ThreadLocalRandom.current();
-        probabilidad.nextDouble(1);
-        return probabilidad.nextDouble() <= 0.9;
+        return random.nextDouble() <= 0.9;
     }
 }
