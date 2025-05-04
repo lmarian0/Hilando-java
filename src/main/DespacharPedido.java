4a39ff4a5ce6feec1f5e8c1d6c23be46d4cea085
RiveraAlex <rivera.joel.alex@gmail.com>
2025-05-04 20:34:10 -0300

    Despachar pedido arreglado
    Incluido synchronized

--- src/main/DespacharPedido.java
+++ src/main/DespacharPedido.java
@@ -8,52 +8,44 @@ public class DespacharPedido extends Proceso{
     public DespacharPedido(EmpresaLogistica eCommerce) {
         super(eCommerce);
     }
+
     @Override
     public void run() {
 
-        /*
-         
-Agarrar un pedido aleatorio de la lista preparacion
-*/
-  Pedido pedido = null;
-
-  try {
-          while(!Thread.currentThread().isInterrupted()) {
-
-                    List<Pedido> lista= eCommerce.getRegistroPedidos().getPreparacion();
-                    if (lista.isEmpty()){
-                    TimeUnit.MILLISECONDS.sleep(500);
+        /*Agarrar un pedido aleatorio de la lista preparacion */
+        Pedido pedido = null;
+        List<Pedido> lista= eCommerce.getRegistroPedidos().getPreparacion();
+        try {
+            while(!Thread.currentThread().isInterrupted()) {
+                    
+                if (lista.isEmpty()){
+                    TimeUnit.MILLISECONDS.sleep(10000);
                     System.out.println("No hay pedidos, a dormir...");
                     continue;
                 }
-
-                int index = ThreadLocalRandom.current().nextInt(lista.size());
-                pedido = lista.get(index);
-                    if (verificarDatos() && pedido.getEstado()==EstadoPedido.EN_PREPARACION){
+                synchronized(eCommerce.getRegistroPedidos()){
+                    int index = ThreadLocalRandom.current().nextInt(lista.size());
+                    pedido = lista.get(index);
+                }
+                if (verificarDatos() && pedido.getEstado()==EstadoPedido.EN_PREPARACION){
                     liberarCasillero(pedido);
-
+                    System.out.println(Thread.currentThread().getName() + " asignó el pedido: " );
                     pedido.setEstado(EstadoPedido.EN_TRANSITO);
                     eCommerce.getRegistroPedidos().delPreparacion(pedido);
                     eCommerce.getRegistroPedidos().addTransito(pedido);
-
                 }
-                    else {
+                else {
                     marcarCasilleroFueraDeServicio(pedido);
                     eCommerce.getRegistroPedidos().delPreparacion(pedido);
                     eCommerce.getRegistroPedidos().addFallidos(pedido);
-
                 }
-
                 TimeUnit.MILLISECONDS.sleep(200);
-
-
-
-
             }
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt(); // Restablecer el estado de interrupción
         }
-}
+    }
 
     private void liberarCasillero(Pedido pedido){
         pedido.getCasilleroAsociado().liberar();
